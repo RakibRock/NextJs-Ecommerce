6342ebf4d90aafbdda45d148424c04d62062df7f
RakibRahman <rakibrahman934@gmail.com>
2021-12-23 00:29:06 +0600

    changes

---
+++ models/Product.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    Category: { type: String, required: true },
+    Image: { type: String, required: true },
+    price: { type: Number, required: true },
+    brand: { type: String, required: true },
+    Rating: { type: Number, required: true, default: 0 },
+    numReviews: { type: Number, required: true, default: 0 },
+    countInStock: { type: Number, required: true, default: 0 },
+    description: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//creating product model
+//condition: if there exits a product then asign the value to product, otherwise create a new model
+const Product =
+  mongoose.models.Product || mongoose.model("Product", productSchema);
+
+export default Product;
